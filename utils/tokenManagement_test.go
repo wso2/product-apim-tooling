29ae7272646654a9d277efd9d483dd130fce19b5
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2017-09-05 00:32:05 +0530

    test: Update Tests for Token Management

--- utils/tokenManagement_test.go
+++ utils/tokenManagement_test.go
@@ -8,56 +8,58 @@ import (
 	"github.com/renstrom/dedent"
 )
 
-func TestGetClientIDSecret(t *testing.T) {
-	one, two, three := GetClientIDSecret("admin", "admin", "")
-	fmt.Println("one:", one)
-	fmt.Println("two:", two)
-	fmt.Println("three:", three)
-}
-
-func TestGetOAuthTokens(t *testing.T) {
-
-}
-
 func TestGetBase64EncodedCredentials(t *testing.T) {
 	usernames := []string{"admin", "user", "admin"}
 	passwords := []string{"admin", "password", "123456"}
 	encodedPairs := []string{"YWRtaW46YWRtaW4=", "dXNlcjpwYXNzd29yZA==", "YWRtaW46MTIzNDU2"}
 
 	for i, s := range encodedPairs {
-		if s != GetBase64EncodedCredentials(usernames[i], passwords[i])	{
+		if s != GetBase64EncodedCredentials(usernames[i], passwords[i]) {
 			t.Errorf("Error in Base64 Encoding. Base64(" + usernames[i] + ":" + passwords[i] + ") = " + encodedPairs[i])
 		}
 	}
 }
 
 func TestGetClientIDSecretUnreachable(t *testing.T) {
+	var registrationStub = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			t.Errorf("Expected 'POST', got '%s'\n", r.Method)
+		}
+		if r.Header.Get(HeaderContentType) != HeaderValueApplicationJSON {
+			t.Errorf("Expected '"+HeaderValueApplicationJSON+"', got '%s'\n", r.Header.Get(HeaderContentType))
+		}
+
+		w.WriteHeader(http.StatusServiceUnavailable)
+		w.Header().Set(HeaderContentType, HeaderValueApplicationJSON)
+	}))
+	defer registrationStub.Close()
+
 	username := "admin"
 	password := "admin"
-	url := "http://localhost:-41"
-	_, _, err := GetClientIDSecret(username, password, url)
+	_, _, err := GetClientIDSecret(username, password, registrationStub.URL)
 	if err == nil {
 		t.Errorf("GetClientIDSecret() didn't return an error")
 	}
 }
 
 func TestGetClientIDSecretOK(t *testing.T) {
-	var registrationStub = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	var registrationStub = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		if r.Method != http.MethodPost {
 			t.Errorf("Expected 'POST', got '%s'\n", r.Method)
 		}
 
+		if r.Header.Get(HeaderContentType) != HeaderValueApplicationJSON {
+			t.Errorf("Expected '"+HeaderValueApplicationJSON+"', got '%s'\n", r.Header.Get(HeaderContentType))
+		}
+
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set(HeaderContentType, HeaderValueApplicationJSON)
-		w.Header().Set("Sample-Header", "Sample-Value")
-		body := dedent.Dedent(`{"clientName":"Test1",
+		body := dedent.Dedent(`{"client_name":"Test1",
 									"client_id":"be88563b-21cb-417c-b574-bf1079959679",
 									"client_secret":"ecb105a0-117c-463d-9376-442d24864f26"}`)
 
-
 		w.Write([]byte(body))
-		//io.WriteString(w, body)
 	}))
 	defer registrationStub.Close()
 
@@ -82,14 +84,63 @@ func TestGetClientIDSecretOK(t *testing.T) {
 }
 
 func TestGetOAuthTokensUnreachable(t *testing.T) {
-	url := "http://localhost:-41"
-	_, err := GetOAuthTokens("","", "", url)
+	var oauthStub = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			t.Errorf("Expected 'POST', got '%s'\n", r.Method)
+		}
+
+		if r.Header.Get(HeaderContentType) != HeaderValueXWWWFormUrlEncoded {
+			t.Errorf("Exptected '"+HeaderValueXWWWFormUrlEncoded+"', got '%s'\n", r.Header.Get(HeaderContentType))
+		}
+
+		w.WriteHeader(http.StatusServiceUnavailable)
+		w.Header().Set(HeaderContentType, HeaderValueApplicationJSON)
+	}))
+	defer oauthStub.Close()
+
+	_, err := GetOAuthTokens("", "", "", oauthStub.URL)
 	if err == nil {
 		t.Errorf("GetOAuthTokens() didn't return an error")
 	}
 }
 
 func TestGetOAuthTokensOK(t *testing.T) {
+	var oauthStub = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			t.Errorf("Expected 'POST', got '%s'\n", r.Method)
+		}
 
+		if r.Header.Get(HeaderContentType) != HeaderValueXWWWFormUrlEncoded {
+			t.Errorf("Expected '"+HeaderValueXWWWFormUrlEncoded+"', got '%s'\n", r.Header.Get(HeaderContentType))
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set(HeaderContentType, HeaderValueApplicationJSON)
+
+		body := dedent.Dedent(`
+			{"access_token":"a2e5c3ac-68e6-4d78-a8a1-b2b0372cb575",
+			 "refresh_token":"fe8f8400-05c9-430f-8e2f-4f3b2fbd01f8",
+			 "expires_in":1487166427829,
+			 "scopes":["apim:api_view","apim:api_create","apim:api_publish",
+				"apim:tier_view","apim:tier_manage","apim:subscription_view",
+				"apim:subscription_block","apim:subscribe"
+				]
+			}
+		`)
+
+		w.Write([]byte(body))
+	}))
+	defer oauthStub.Close()
+
+	m, err := GetOAuthTokens("admin", "admin", "", oauthStub.URL)
+	if err != nil {
+		t.Error("Error in GetOAuthTokens()")
+	}
+
+	if m["refresh_token"] != "fe8f8400-05c9-430f-8e2f-4f3b2fbd01f8"{
+		t.Error("Error in GetOAuthTokens(): Incorrect RefreshToken")
+	}
+	if m["access_token"] != "a2e5c3ac-68e6-4d78-a8a1-b2b0372cb575"{
+		t.Error("Error in GetOAuthTokens(): Incorrect AccessToken")
+	}
 }
-
