ed3657d1c0402ce1d939224947bda78e250133c5
Akila94 <akilaaroshana@hotmail.com>
2017-11-13 11:15:28 +0530

    Removed unused import List

--- src/main/java/com/swagger/plugins/wso2/WSO2ApiGatewayPlugin.java
+++ src/main/java/com/swagger/plugins/wso2/WSO2ApiGatewayPlugin.java
@@ -26,8 +26,6 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
-
 
 /*****************************************************************
  *  Class name :   WSO2ApiGatewayPlugin
