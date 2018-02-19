18c1c00e59412959fba53a178acdb527e0514dfb
randilu <randilu.s@gmail.com>
2018-02-19 11:28:26 +0530

    Adding bash-completions for application list/export/import cmds

--- import-export-cli/apimcli_bash_completion.sh
+++ import-export-cli/apimcli_bash_completion.sh
@@ -5,11 +5,16 @@ import_export_cli()
     current="${COMP_WORDS[COMP_CWORD]}"
     previous="${COMP_WORDS[COMP_CWORD-1]}"
 
-    options="export-api import-api list add-env remove-env reset-user version author"
+    options="export-api export-app import-api import-app list add-env remove-env reset-user version author"
 
     case "${previous}" in
         export-api)
             local flags="--name -n --version -v --environment -e --help -h"
+            COMPREPLY=( $(compgen -W "${flags}" -- ${current}) )
+            return 0
+            ;;
+        export-app)
+            local flags="--name -n --environment -e --help -h"
             COMPREPLY=( $(compgen -W "${flags}" -- ${current}) )
             return 0
             ;;
@@ -18,8 +23,13 @@ import_export_cli()
             COMPREPLY=( $(compgen -W "${flags}" -- ${current}) )
             return 0
             ;;
+        import-app)
+            local flags="--file -f --environment -e --help -h"
+            COMPREPLY=( $(compgen -W "${flags}" -- ${current}) )
+            return 0
+            ;;
         list)
-            local flags="apis envs"
+            local flags="apis apps envs"
             COMPREPLY=( $(compgen -W "${flags}" -- ${current}) )
             return 0
             ;;
