ff454eb5a48cb53eb7a47f22fee5b1cb4b45e373
Kasun Vithanage <alankasun@gmail.com>
2019-03-22 10:53:00 +0530

    remove old archive code and replace it with stdlib version

--- import-export-cli/utils/zipUtils.go
+++ import-export-cli/utils/zipUtils.go
@@ -19,23 +19,71 @@
 package utils
 
 import (
-	"github.com/jhoonb/archivex"
+	"archive/zip"
+	"io"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
+// ZipDir will create an archive from source and store it in target
 func ZipDir(source, target string) error {
-	err := os.Chdir(source)
-	if err == nil {
-		Logln(LogPrefixInfo + "Directory " + source + " exists")
-		Logln(LogPrefixInfo + "Starting Compression...")
-		zip := new(archivex.ZipFile)
-		zip.Create(target)
-		zip.AddAll(source, true)
-		zip.Close()
-		Logln(LogPrefixInfo + "Compression completed: Find file " + target)
-		return nil
-	} else {
-		Logln(LogPrefixError + "Compressing " + source)
+	zipFile, err := os.Create(target)
+	if err != nil {
 		return err
 	}
+	defer zipFile.Close()
+
+	archive := zip.NewWriter(zipFile)
+	defer archive.Close()
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return nil
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		if baseDir != "" {
+			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+		}
+
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		writer, err := archive.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = io.Copy(writer, file)
+		return err
+	})
+
+	return err
 }
