3463dfbf97e14a5cc9b78c40802ed660cc3e42f0
Nipun Sampath <nipunsampath@gmail.com>
2020-02-10 11:33:13 +0530

    Added constants module to avoid duplication of general constants

---
+++ apim-traffic-tool/lib/attack-tool/src/python/constants.py
@@ -0,0 +1,19 @@
+# Constants
+
+GENERAL_CONFIG = 'general_config'
+API_HOST = 'api_host'
+NUMBER_OF_PROCESSES = 'number_of_processes'
+USER_AGENTS = 'user_agents'
+PAYLOADS = 'payloads'
+ATTACK_DURATION = 'attack_duration'
+PORT = 'port'
+IP = 'ip'
+PROTOCOL = 'protocol'
+COMPROMISED_USER_COUNT = 'compromised_user_count'
+STOLEN_TOKEN = 'stolen_token'
+ATTACKS = 'attacks'
+TIME_PATTERNS = 'time_patterns'
+
+
+
+
