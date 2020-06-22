9bcf3357ba14114eff3bab114b9c303ce7aa2312
wasuradananjith <wasuradananjith@gmail.com>
2020-06-22 11:29:45 +0530

    Refactor old variable names to have more meaning

--- import-export-cli/impl/importAPI.go
+++ import-export-cli/impl/importAPI.go
@@ -49,7 +49,7 @@ import (
 )
 
 var (
-	reApiName = regexp.MustCompile(`[~!@#;:%^*()+={}|\\<>"',&/$]`)
+	reAPIName = regexp.MustCompile(`[~!@#;:%^*()+={}|\\<>"',&/$]`)
 )
 
 // extractAPIDefinition extracts API information from jsonContent
@@ -72,7 +72,7 @@ func getAPIDefinition(filePath string) (*v2.APIDefinition, []byte, error) {
 
 	var buffer []byte
 	if info.IsDir() {
-		_, content, err := resolveYamlOrJson(path.Join(filePath, "Meta-information", "api"))
+		_, content, err := resolveYamlOrJSON(path.Join(filePath, "Meta-information", "api"))
 		if err != nil {
 			return nil, nil, err
 		}
@@ -93,7 +93,7 @@ func mergeAPI(apiDirectory string, environmentParams *params.Environment) error 
 	// read api from Meta-information
 	apiPath := filepath.Join(apiDirectory, "Meta-information", "api")
 	utils.Logln(utils.LogPrefixInfo + "Reading API definition: ")
-	fp, jsonContent, err := resolveYamlOrJson(apiPath)
+	fp, jsonContent, err := resolveYamlOrJSON(apiPath)
 	if err != nil {
 		return err
 	}
@@ -231,10 +231,10 @@ func setupMultipleEndpoints(environmentParams *params.Environment) ([]byte, erro
 			// The default class of the algorithm to be used should be set to RoundRobin
 			environmentParams.LoadBalanceEndpoints.AlgorithmClassName = utils.LoadBalanceAlgorithmClass
 			environmentParams.LoadBalanceEndpoints.EndpointType = utils.LoadBalanceEndpointTypeForJSON
-			for index, _ := range environmentParams.LoadBalanceEndpoints.Production {
+			for index := range environmentParams.LoadBalanceEndpoints.Production {
 				environmentParams.LoadBalanceEndpoints.Production[index].EndpointType = utils.HttpSOAPEndpointTypeForJSON
 			}
-			for index, _ := range environmentParams.LoadBalanceEndpoints.Sandbox {
+			for index := range environmentParams.LoadBalanceEndpoints.Sandbox {
 				environmentParams.LoadBalanceEndpoints.Sandbox[index].EndpointType = utils.HttpSOAPEndpointTypeForJSON
 			}
 			configData, err = json.Marshal(environmentParams.LoadBalanceEndpoints)
@@ -247,10 +247,10 @@ func setupMultipleEndpoints(environmentParams *params.Environment) ([]byte, erro
 			}
 			environmentParams.FailoverEndpoints.Production.EndpointType = utils.HttpSOAPEndpointTypeForJSON
 			environmentParams.FailoverEndpoints.Sandbox.EndpointType = utils.HttpSOAPEndpointTypeForJSON
-			for index, _ := range environmentParams.FailoverEndpoints.ProductionFailovers {
+			for index := range environmentParams.FailoverEndpoints.ProductionFailovers {
 				environmentParams.FailoverEndpoints.ProductionFailovers[index].EndpointType = utils.HttpSOAPEndpointTypeForJSON
 			}
-			for index, _ := range environmentParams.FailoverEndpoints.SandboxFailovers {
+			for index := range environmentParams.FailoverEndpoints.SandboxFailovers {
 				environmentParams.FailoverEndpoints.SandboxFailovers[index].EndpointType = utils.HttpSOAPEndpointTypeForJSON
 			}
 			environmentParams.FailoverEndpoints.EndpointType = utils.FailoverRoutingPolicy
@@ -438,7 +438,7 @@ func resolveAPIParamsPath(importPath, paramPath string) (string, error) {
 	}
 }
 
-func getTempApiDirectory(file string) (string, error) {
+func getTempAPIDirectory(file string) (string, error) {
 	fileIsDir := false
 	// create a temp directory
 	tmpDir, err := ioutil.TempDir("", "apim")
@@ -472,11 +472,11 @@ func getTempApiDirectory(file string) (string, error) {
 	}
 }
 
-// resolveYamlOrJson for a given filepath.
+// resolveYamlOrJSON for a given filepath.
 // first it will look for the yaml file, if not will fallback for json
 // give filename without extension so resolver will resolve for file
 // fn is resolved filename, jsonContent is file as a json object, error if anything wrong happen(or both files does not exists)
-func resolveYamlOrJson(filename string) (string, []byte, error) {
+func resolveYamlOrJSON(filename string) (string, []byte, error) {
 	// lookup for yaml
 	yamlFp := filename + ".yaml"
 	if info, err := os.Stat(yamlFp); err == nil && !info.IsDir() {
@@ -605,9 +605,9 @@ func injectParamsToAPI(importPath, paramsPath, importEnvironment string) error {
 	return nil
 }
 
-// getApiID returns id of the API by using apiInfo which contains name and version as info
-func getApiID(accessOAuthToken, environment, name, version string) (string, error) {
-	apiQuery := fmt.Sprintf("name:\"%s\" version:\"%s\"", name, version)
+// getAPIID returns id of the API by using apiInfo which contains name and version as info
+func getAPIID(accessOAuthToken, environment, name, version string) (string, error) {
+	apiQuery := fmt.Sprintf("name:%s version:%s", name, version)
 	count, apis, err := GetAPIListFromEnv(accessOAuthToken, environment, url.QueryEscape(apiQuery), "")
 	if err != nil {
 		return "", err
@@ -625,7 +625,7 @@ func isEmpty(s string) bool {
 
 func preProcessAPI(apiDirectory string) error {
 	dirty := false
-	apiPath, jsonData, err := resolveYamlOrJson(filepath.Join(apiDirectory, "Meta-information", "api"))
+	apiPath, jsonData, err := resolveYamlOrJSON(filepath.Join(apiDirectory, "Meta-information", "api"))
 	if err != nil {
 		return err
 	}
@@ -674,14 +674,14 @@ func preProcessAPI(apiDirectory string) error {
 	}
 
 	if dirty {
-		yamlApiPath := filepath.Join(apiDirectory, "Meta-information", "api.yaml")
-		utils.Logln(utils.LogPrefixInfo+"Writing preprocessed API to:", yamlApiPath)
+		yamlAPIPath := filepath.Join(apiDirectory, "Meta-information", "api.yaml")
+		utils.Logln(utils.LogPrefixInfo+"Writing preprocessed API to:", yamlAPIPath)
 		content, err := utils.JsonToYaml(api.Bytes())
 		if err != nil {
 			return err
 		}
 		// write this to disk
-		err = ioutil.WriteFile(yamlApiPath, content, 0644)
+		err = ioutil.WriteFile(yamlAPIPath, content, 0644)
 		if err != nil {
 			return err
 		}
@@ -716,7 +716,7 @@ func replaceEnvVariables(apiFilePath string) error {
 	return nil
 }
 
-func populateApiWithDefaults(def *v2.APIDefinition) (dirty bool) {
+func populateAPIWithDefaults(def *v2.APIDefinition) (dirty bool) {
 	dirty = false
 	if def.ContextTemplate == "" {
 		if !strings.Contains(def.Context, "{version}") {
@@ -744,13 +744,13 @@ func populateApiWithDefaults(def *v2.APIDefinition) (dirty bool) {
 	return
 }
 
-// validateApiDefinition validates an API against basic rules
-func validateApiDefinition(def *v2.APIDefinition) error {
+// validateAPIDefinition validates an API against basic rules
+func validateAPIDefinition(def *v2.APIDefinition) error {
 	utils.Logln(utils.LogPrefixInfo + "Validating API")
 	if isEmpty(def.ID.APIName) {
 		return errors.New("apiName is required")
 	}
-	if reApiName.MatchString(def.ID.APIName) {
+	if reAPIName.MatchString(def.ID.APIName) {
 		return errors.New(`apiName contains one or more illegal characters (~!@#;:%^*()+={}|\\<>"',&\/$)`)
 	}
 
@@ -878,14 +878,14 @@ func ImportAPIToEnv(accessOAuthToken, importEnvironment, importPath, apiParamsPa
 func ImportAPI(accessOAuthToken, adminEndpoint, importEnvironment, importPath, apiParamsPath string, importAPIUpdate, preserveProvider,
 	importAPISkipCleanup bool) error {
 	exportDirectory := filepath.Join(utils.ExportDirectory, utils.ExportedApisDirName)
-	resolvedApiFilePath, err := resolveImportFilePath(importPath, exportDirectory)
+	resolvedAPIFilePath, err := resolveImportFilePath(importPath, exportDirectory)
 	if err != nil {
 		return err
 	}
-	utils.Logln(utils.LogPrefixInfo+"API Location:", resolvedApiFilePath)
+	utils.Logln(utils.LogPrefixInfo+"API Location:", resolvedAPIFilePath)
 
 	utils.Logln(utils.LogPrefixInfo + "Creating workspace")
-	tmpPath, err := getTempApiDirectory(resolvedApiFilePath)
+	tmpPath, err := getTempAPIDirectory(resolvedAPIFilePath)
 	if err != nil {
 		return err
 	}
@@ -915,7 +915,7 @@ func ImportAPI(accessOAuthToken, adminEndpoint, importEnvironment, importPath, a
 	}
 
 	utils.Logln(utils.LogPrefixInfo + "Attempting to inject parameters to the API from api_params.yaml (if exists)")
-	paramsPath, err := resolveAPIParamsPath(resolvedApiFilePath, apiParamsPath)
+	paramsPath, err := resolveAPIParamsPath(resolvedAPIFilePath, apiParamsPath)
 	if err != nil && apiParamsPath != utils.ParamFileAPI && apiParamsPath != "" {
 		return err
 	}
@@ -933,7 +933,7 @@ func ImportAPI(accessOAuthToken, adminEndpoint, importEnvironment, importPath, a
 		return err
 	}
 	// Fill with defaults
-	if populateApiWithDefaults(apiInfo) {
+	if populateAPIWithDefaults(apiInfo) {
 		utils.Logln(utils.LogPrefixInfo + "API is populated with defaults")
 		// api is dirty, write it to disk
 		buf, err := json.Marshal(apiInfo)
@@ -967,7 +967,7 @@ func ImportAPI(accessOAuthToken, adminEndpoint, importEnvironment, importPath, a
 		}
 	}
 	// validate definition
-	if err = validateApiDefinition(apiInfo); err != nil {
+	if err = validateAPIDefinition(apiInfo); err != nil {
 		return err
 	}
 
@@ -999,7 +999,7 @@ func ImportAPI(accessOAuthToken, adminEndpoint, importEnvironment, importPath, a
 	updateAPI := false
 	if importAPIUpdate {
 		// check for API existence
-		id, err := getApiID(accessOAuthToken, importEnvironment, apiInfo.ID.APIName, apiInfo.ID.Version)
+		id, err := getAPIID(accessOAuthToken, importEnvironment, apiInfo.ID.APIName, apiInfo.ID.Version)
 		if err != nil {
 			return err
 		}
--- import-export-cli/impl/importAPIProduct.go
+++ import-export-cli/impl/importAPIProduct.go
@@ -41,7 +41,7 @@ import (
 )
 
 var (
-	reApiProductName = regexp.MustCompile(`[~!@#;:%^*()+={}|\\<>"',&/$]`)
+	reAPIProductName = regexp.MustCompile(`[~!@#;:%^*()+={}|\\<>"',&/$]`)
 )
 
 // extractAPIProductDefinition extracts API Product information from jsonContent
@@ -64,7 +64,7 @@ func getAPIProductDefinition(filePath string) (*v2.APIProductDefinition, []byte,
 
 	var buffer []byte
 	if info.IsDir() {
-		_, content, err := resolveYamlOrJson(path.Join(filePath, "Meta-information", "api"))
+		_, content, err := resolveYamlOrJSON(path.Join(filePath, "Meta-information", "api"))
 		if err != nil {
 			return nil, nil, err
 		}
@@ -100,9 +100,9 @@ func resolveImportAPIProductFilePath(file, defaultExportDirectory string) (strin
 	return absPath, nil
 }
 
-// getApiProductID returns id of the API Product by using apiProductInfo which contains name, version and provider as info
-func getApiProductID(name, version, environment, accessOAuthToken string) (string, error) {
-	apiProductQuery := fmt.Sprintf("name:\"%s\" version:\"%s\"", name, version)
+// getAPIProductID returns id of the API Product by using apiProductInfo which contains name, version and provider as info
+func getAPIProductID(name, version, environment, accessOAuthToken string) (string, error) {
+	apiProductQuery := fmt.Sprintf("name:%s version:%s", name, version)
 	apiProductQuery += " type:\"" + utils.DefaultApiProductType + "\""
 	count, apiProducts, err := GetAPIProductListFromEnv(accessOAuthToken, environment, url.QueryEscape(apiProductQuery), "")
 	if err != nil {
@@ -114,7 +114,7 @@ func getApiProductID(name, version, environment, accessOAuthToken string) (strin
 	return apiProducts[0].ID, nil
 }
 
-func populateApiProductWithDefaults(def *v2.APIProductDefinition) (dirty bool) {
+func populateAPIProductWithDefaults(def *v2.APIProductDefinition) (dirty bool) {
 	dirty = false
 	if def.ContextTemplate == "" {
 		if !strings.Contains(def.Context, "{version}") {
@@ -135,12 +135,12 @@ func populateApiProductWithDefaults(def *v2.APIProductDefinition) (dirty bool) {
 }
 
 // validateApiProductDefinition validates an API Product against basic rules
-func validateApiProductDefinition(def *v2.APIProductDefinition) error {
+func validateAPIProductDefinition(def *v2.APIProductDefinition) error {
 	utils.Logln(utils.LogPrefixInfo + "Validating API Product")
 	if isEmpty(def.ID.APIProductName) {
 		return errors.New("apiProductName is required")
 	}
-	if reApiProductName.MatchString(def.ID.APIProductName) {
+	if reAPIProductName.MatchString(def.ID.APIProductName) {
 		return errors.New(`apiProductName contains one or more illegal characters (~!@#;:%^*()+={}|\\<>"',&\/$)`)
 	}
 	if isEmpty(def.ID.Version) {
@@ -263,14 +263,14 @@ func ImportAPIProduct(accessOAuthToken, adminEndpoint, importEnvironment, import
 	importAPIProductUpdate, importAPIProductPreserveProvider, importAPIProductSkipCleanup bool) error {
 	var exportDirectory = filepath.Join(utils.ExportDirectory, utils.ExportedApiProductsDirName)
 
-	resolvedApiProductFilePath, err := resolveImportAPIProductFilePath(importPath, exportDirectory)
+	resolvedAPIProductFilePath, err := resolveImportAPIProductFilePath(importPath, exportDirectory)
 	if err != nil {
 		return err
 	}
-	utils.Logln(utils.LogPrefixInfo+"API Product Location:", resolvedApiProductFilePath)
+	utils.Logln(utils.LogPrefixInfo+"API Product Location:", resolvedAPIProductFilePath)
 
 	utils.Logln(utils.LogPrefixInfo + "Creating workspace")
-	tmpPath, err := getTempApiDirectory(resolvedApiProductFilePath)
+	tmpPath, err := getTempAPIDirectory(resolvedAPIProductFilePath)
 	if err != nil {
 		return err
 	}
@@ -305,7 +305,7 @@ func ImportAPIProduct(accessOAuthToken, adminEndpoint, importEnvironment, import
 		return err
 	}
 	// Fill with defaults
-	if populateApiProductWithDefaults(apiProductInfo) {
+	if populateAPIProductWithDefaults(apiProductInfo) {
 		utils.Logln(utils.LogPrefixInfo + "API Product is populated with defaults")
 		// API Product is dirty, write it to disk
 		buf, err := json.Marshal(apiProductInfo)
@@ -339,7 +339,7 @@ func ImportAPIProduct(accessOAuthToken, adminEndpoint, importEnvironment, import
 		}
 	}
 	// Validate definition
-	if err = validateApiProductDefinition(apiProductInfo); err != nil {
+	if err = validateAPIProductDefinition(apiProductInfo); err != nil {
 		return err
 	}
 
@@ -371,7 +371,7 @@ func ImportAPIProduct(accessOAuthToken, adminEndpoint, importEnvironment, import
 	updateAPIProduct := false
 	if importAPIsUpdate || importAPIProductUpdate {
 		// Check for API Product existence
-		id, err := getApiProductID(apiProductInfo.ID.APIProductName, apiProductInfo.ID.Version, importEnvironment, accessOAuthToken)
+		id, err := getAPIProductID(apiProductInfo.ID.APIProductName, apiProductInfo.ID.Version, importEnvironment, accessOAuthToken)
 		if err != nil {
 			return err
 		}
