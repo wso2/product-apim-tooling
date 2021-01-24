0d6de3c7bf9d9d056b1d1e876b44c31cd0216e43
Sanoj Punchihewa <sanoj.punchihewa@gmail.com>
2021-01-24 15:57:38 +0530

    Add integration tests for mi activate proxy-service command

--- import-export-cli/mi/integration/proxyService_test.go
+++ import-export-cli/mi/integration/proxyService_test.go
@@ -28,34 +28,61 @@ import (
 
 const validProxyServiceName = "StockQuoteProxy"
 const invalidProxyServiceName = "abc-proxy"
-const proxyServiceCmd = "proxy-services"
+const proxyServicesCmd = "proxy-services"
+const proxyServiceCmd = "proxy-service"
 
 func TestGetProxyServices(t *testing.T) {
-	testutils.ValidateProxyServiceList(t, proxyServiceCmd, config)
+	testutils.ValidateProxyServiceList(t, proxyServicesCmd, config)
 }
 
 func TestGetProxyServiceByName(t *testing.T) {
-	testutils.ValidateProxyService(t, proxyServiceCmd, config, validProxyServiceName)
+	testutils.ValidateProxyService(t, proxyServicesCmd, config, validProxyServiceName)
 }
 
 func TestGetNonExistingProxyServiceByName(t *testing.T) {
-	response, _ := testutils.GetArtifact(t, proxyServiceCmd, invalidProxyServiceName, config)
+	response, _ := testutils.GetArtifact(t, proxyServicesCmd, invalidProxyServiceName, config)
 	base.Log(response)
 	assert.Contains(t, response, "[ERROR]: Getting Information of proxy services [ "+invalidProxyServiceName+" ]  404 Not Found")
 }
 
 func TestGetProxyServicesWithoutSettingUpEnv(t *testing.T) {
-	testutils.ExecGetCommandWithoutSettingEnv(t, proxyServiceCmd)
+	testutils.ExecGetCommandWithoutSettingEnv(t, proxyServicesCmd)
 }
 
 func TestGetProxyServicesWithoutLogin(t *testing.T) {
-	testutils.ExecGetCommandWithoutLogin(t, proxyServiceCmd, config)
+	testutils.ExecGetCommandWithoutLogin(t, proxyServicesCmd, config)
 }
 
 func TestGetProxyServicesWithoutEnvFlag(t *testing.T) {
-	testutils.ExecGetCommandWithoutEnvFlag(t, proxyServiceCmd, config)
+	testutils.ExecGetCommandWithoutEnvFlag(t, proxyServicesCmd, config)
 }
 
 func TestGetProxyServicesWithInvalidArgs(t *testing.T) {
-	testutils.ExecGetCommandWithInvalidArgCount(t, config, 1, 2, false, proxyServiceCmd, validProxyServiceName, invalidProxyServiceName)
+	testutils.ExecGetCommandWithInvalidArgCount(t, config, 1, 2, false, proxyServicesCmd, validProxyServiceName, invalidProxyServiceName)
+}
+
+func TestActivateProxyService(t *testing.T) {
+	expected := validProxyServiceName + " started successfully"
+	testutils.ExecActivateCommand(t, config, proxyServiceCmd, validProxyServiceName, expected)
+}
+
+func TestActivateNonExistingProxyService(t *testing.T) {
+	expected := "[ERROR]: Activating proxy service [ " + invalidProxyServiceName + " ] Proxy service could not be found"
+	testutils.ExecActivateCommand(t, config, proxyServiceCmd, invalidProxyServiceName, expected)
+}
+
+func TestActivateProxyServiceWithoutEnvFlag(t *testing.T) {
+	testutils.ExecActivateCommandWithoutEnvFlag(t, config, proxyServiceCmd, validProxyServiceName)
+}
+
+func TestActivateProxyServiceWithInvalidArgs(t *testing.T) {
+	testutils.ExecActivateCommandWithInvalidArgCount(t, config, 1, 0, proxyServiceCmd)
+}
+
+func TestActivateProxyServiceWithoutSettingUpEnv(t *testing.T) {
+	testutils.ExecActivateCommandWithoutSettingEnv(t, proxyServiceCmd, validProxyServiceName)
+}
+
+func TestActivateProxyServiceWithoutLogin(t *testing.T) {
+	testutils.ExecActivateCommandWithoutLogin(t, config, proxyServiceCmd, validProxyServiceName)
 }
