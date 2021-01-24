189931fed9a0aa87f9b7b5652e37640c2a669b6b
Sanoj Punchihewa <sanoj.punchihewa@gmail.com>
2021-01-24 20:00:29 +0530

    Add integration tests for mi update log-level command

--- import-export-cli/mi/integration/logLevel_test.go
+++ import-export-cli/mi/integration/logLevel_test.go
@@ -33,6 +33,7 @@ const newLoggerName = "synapse-api"
 const newLoggerClass = "org.apache.synapse.rest.API"
 
 var validAddLoggerCmd = []string{"mi", "add", "log-level", newLoggerName, newLoggerClass, "DEBUG", "-e", "testing"}
+var validUpdateLoggerCmd = []string{"mi", "update", "log-level", newLoggerName, "INFO", "-e", "testing"}
 
 func TestGetLoggerByName(t *testing.T) {
 	testutils.ValidateLogger(t, logLevelCmd, config, validLoggerName)
@@ -117,5 +118,65 @@ func TestAddExistingLogger(t *testing.T) {
 	response, _ := base.Execute(t, validAddLoggerCmd...)
 	base.Log(response)
 	expected := "[ERROR]: Adding new logger [ " + newLoggerName + " ]  Specified logger name ('" + newLoggerName + "') already exists, try updating the level instead"
+	assert.Contains(t, response, expected)
+}
+
+func TestUpdateLoggerWithInvalidLogLevel(t *testing.T) {
+	base.SetupMIEnv(t, config.MIClient.GetEnvName(), config.MIClient.GetMiURL())
+	base.MILogin(t, config.MIClient.GetEnvName(), config.Username, config.Username)
+	response, _ := base.Execute(t, "mi", "update", "log-level", newLoggerName, "ABC", "-e", "testing")
+	base.Log(response)
+	expected := "[ERROR]: updating logger [ " + newLoggerName + " ]  Invalid log level ABC"
+	assert.Contains(t, response, expected)
+}
+
+func TestUpdateLoggerWithoutEnvFlag(t *testing.T) {
+	base.SetupMIEnv(t, config.MIClient.GetEnvName(), config.MIClient.GetMiURL())
+	base.MILogin(t, config.MIClient.GetEnvName(), config.Username, config.Username)
+	response, _ := base.Execute(t, "mi", "update", "log-level", newLoggerName, "INFO")
+	base.Log(response)
+	expected := `required flag(s) "environment" not set`
+	assert.Contains(t, response, expected)
+}
+
+func TestUpdateLoggerWithInvalidArgs(t *testing.T) {
+	base.SetupMIEnv(t, config.MIClient.GetEnvName(), config.MIClient.GetMiURL())
+	base.MILogin(t, config.MIClient.GetEnvName(), config.Username, config.Username)
+	response, _ := base.Execute(t, "mi", "update", "log-level", newLoggerName, "-e", "testing")
+	base.Log(response)
+	expected := "accepts 2 arg(s), received 1"
+	assert.Contains(t, response, expected)
+}
+
+func TestUpdateLoggerWithoutSettingUpEnv(t *testing.T) {
+	response, _ := base.Execute(t, validUpdateLoggerCmd...)
+	base.GetRowsFromTableResponse(response)
+	base.Log(response)
+	assert.Contains(t, response, "MI does not exists in testing Add it using add env")
+}
+
+func TestUpdateLoggerWithoutLogin(t *testing.T) {
+	base.SetupMIEnv(t, config.MIClient.GetEnvName(), config.MIClient.GetMiURL())
+	response, _ := base.Execute(t, validUpdateLoggerCmd...)
+	base.GetRowsFromTableResponse(response)
+	base.Log(response)
+	assert.Contains(t, response, "Login to MI")
+}
+
+func TestUpdateLogger(t *testing.T) {
+	base.SetupMIEnv(t, config.MIClient.GetEnvName(), config.MIClient.GetMiURL())
+	base.MILogin(t, config.MIClient.GetEnvName(), config.Username, config.Username)
+	response, _ := base.Execute(t, validUpdateLoggerCmd...)
+	base.Log(response)
+	expected := "Successfully added logger for ('" + newLoggerName + "') with level INFO"
+	assert.Contains(t, response, expected)
+}
+
+func TestUpdateLoggerNonExistingLogger(t *testing.T) {
+	base.SetupMIEnv(t, config.MIClient.GetEnvName(), config.MIClient.GetMiURL())
+	base.MILogin(t, config.MIClient.GetEnvName(), config.Username, config.Username)
+	response, _ := base.Execute(t, "mi", "update", "log-level", invalidLoggerName, "INFO", "-e", "testing")
+	base.Log(response)
+	expected := "[ERROR]: updating logger [ " + invalidLoggerName + " ]  Specified logger ('" + invalidLoggerName + "') not found"
 	assert.Contains(t, response, expected)
 }
