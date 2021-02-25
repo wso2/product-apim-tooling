23275a2212c16cc6e14281e061cddb3264c28445
wasuradananjith <wasuradananjith@gmail.com>
2021-02-25 11:00:35 +0530

    Eleminate adding LICENSE file to the archives

--- import-export-cli/build.sh
+++ import-export-cli/build.sh
@@ -109,7 +109,6 @@ do
     mkdir -p $zipdir
 
     cp -r "${baseDir}/resources/README.html" $zipdir > /dev/null 2>&1
-    cp -r "${baseDir}/LICENSE" $zipdir > /dev/null 2>&1
     cp -r "${baseDir}/LICENSE.txt" $zipdir > /dev/null 2>&1
 
     # set destination path for binary
