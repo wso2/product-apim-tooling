f409539853a336ad9465b7ecfbc34812dd4d9768
uvindra <uvindra_dj@yahoo.com>
2021-03-11 17:59:10 +0530

    Revert Override testing APIM Host/Port via env var
    This reverts commit bac26d65711c7957127d5eb258386cb3754e6e1e.

--- import-export-cli/integration/integration_test.go
+++ import-export-cli/integration/integration_test.go
@@ -121,7 +121,6 @@ func readConfigs() {
 	yaml.NewDecoder(reader).Decode(&yamlConfig)
 
 	for _, env := range yamlConfig.Environments {
-		resolveEnvVariables(&env)
 		envs[env.Name] = env
 	}
 
@@ -137,26 +136,6 @@ func readConfigs() {
 
 	if len(envs) != 2 {
 		base.Fatal("Expected number of Envs have not been configured for intergration tests")
-	}
-}
-
-func resolveEnvVariables(env *Environment) {
-	host := os.Getenv(env.Name + "_" + "HOST")
-
-	if host != "" {
-		env.Host = host
-	}
-
-	offset := os.Getenv(env.Name + "_" + "OFFSET")
-
-	if offset != "" {
-		value, err := strconv.Atoi(offset)
-
-		if err != nil {
-			base.Fatal(err)
-		}
-
-		env.Offset = value
 	}
 }
 
