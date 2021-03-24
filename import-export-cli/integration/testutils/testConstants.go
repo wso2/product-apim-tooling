bf983a5e497d1da18f3c809247cee8894a4cb81b
wasuradananjith <wasuradananjith@gmail.com>
2021-03-24 18:32:17 +0530

    Remove unnecessary API definition and improve tests

--- import-export-cli/integration/testutils/testConstants.go
+++ import-export-cli/integration/testutils/testConstants.go
@@ -23,7 +23,6 @@ const TestSwagger2DefinitionPath = "testdata/swagger2Definition.yaml"
 const TestOpenAPI3DefinitionPath = "testdata/openAPI3Definition.yaml"
 const TestOpenAPISpecificationURL = "https://petstore.swagger.io/v2/swagger.json"
 const TestMigrationDirectorySuffix = "/migration"
-const TestApiDefinitionPath = "testdata/testAPIDefinition.yaml"
 
 const DefaultApictlTestAppName = "default-apictl-app"
 
