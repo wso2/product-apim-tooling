8eb63df0c48c709e206d94f33b3f94a0365e03fd
wasuradananjith <wasuradananjith@gmail.com>
2021-04-21 14:13:51 +0530

    Remove version from undeploy api-product examples

--- import-export-cli/cmd/undeployApiProduct.go
+++ import-export-cli/cmd/undeployApiProduct.go
@@ -47,12 +47,12 @@ const undeployAPIProductCmdExamples = utils.
 	ProjectName + ` ` + UndeployCmdLiteral + ` ` + UndeployAPIProductCmdLiteral + ` -n TwitterAPIProduct --rev 2  -e dev
 ` + utils.ProjectName + ` ` + UndeployCmdLiteral + ` ` + UndeployAPIProductCmdLiteral + ` -n StoreProduct --rev 6 -g Label1 -g Label2 -g Label3 -e production
 ` + utils.ProjectName + ` ` + UndeployCmdLiteral + ` ` + UndeployAPIProductCmdLiteral + ` -n FacebookProduct -r admin --rev 2 -g Label1 -e production
-NOTE: All 4 flags (--name (-n), --version (-v), --rev, --environment (-e)) are mandatory.
+NOTE: All 3 flags (--name (-n), --rev, --environment (-e)) are mandatory.
 If the flag (--gateway-env (-g)) is not provided, revision will be undeployed from all deployed gateway environments.`
 
 // UndeployAPIProductCmd represents the deploy API command
 var UndeployAPIProductCmd = &cobra.Command{
-	Use: UndeployAPIProductCmdLiteral + " (--name <name-of-the-api-product> --version <version-of-the-api-product> " +
+	Use: UndeployAPIProductCmdLiteral + " (--name <name-of-the-api-product> " +
 		"--rev<revision-number-of-the-api-product> --gateway-env <gateway-environment> " +
 		"--environment <environment-from-which-the-api-product-should-be-undeployed>)",
 	Short:   undeployAPIProductCmdShortDesc,
