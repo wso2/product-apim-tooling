f46ef246a1d2cac04f8ad7fa885d92f14ce797f0
Chamindu36 <cudakara@gmail.com>
2021-06-10 09:23:02 +0530

    Add limit param to api list command

--- import-export-cli/cmd/exportAPIs.go
+++ import-export-cli/cmd/exportAPIs.go
@@ -259,7 +259,7 @@ func getAPIList() (count int32, apis []utils.API) {
 		if cmdResourceTenantDomain != "" {
 			apiListEndpoint += "&tenantDomain=" + cmdResourceTenantDomain
 		}
-		count, apis, err := GetAPIList("", accessToken, apiListEndpoint)
+		count, apis, err := GetAPIList("", "", accessToken, apiListEndpoint)
 		if err == nil {
 			return count, apis
 		} else {
