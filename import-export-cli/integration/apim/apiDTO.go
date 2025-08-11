046d7299a448fda2d8c4355996a95c31a3255fe4
HeshanSudarshana <khsudarshana@gmail.com>
2025-08-11 08:31:54 +0530

    Add displayName and initatedFromGateway fields to testcases

--- import-export-cli/integration/apim/apiDTO.go
+++ import-export-cli/integration/apim/apiDTO.go
@@ -28,6 +28,7 @@ type APIFile struct {
 type API struct {
 	ID                              string            `json:"id,omitempty" yaml:"id,omitempty"`
 	Name                            string            `json:"name,omitempty" yaml:"name,omitempty"`
+	DisplayName                     string            `json:"displayName,omitempty" yaml:"displayName,omitempty"`
 	Description                     string            `json:"description,omitempty" yaml:"description,omitempty"`
 	Context                         string            `json:"context,omitempty" yaml:"context,omitempty"`
 	Version                         string            `json:"version,omitempty" yaml:"version,omitempty"`
@@ -79,6 +80,7 @@ type API struct {
 	GatewayVendor                   string            `json:"gatewayVendor,omitempty" yaml:"gatewayVendor,omitempty"`
 	AsyncTransportProtocols         []string          `json:"asyncTransportProtocols,omitempty" yaml:"asyncTransportProtocols,omitempty"`
 	GatewayType                     string            `json:"gatewayType,omitempty" yaml:"gatewayType,omitempty"`
+	InitiatedFromGateway            bool              `json:"initiatedFromGateway,omitempty" yaml:"initiatedFromGateway,omitempty"`
 	EnableSubscriberVerification    bool              `json:"enableSubscriberVerification,omitempty" yaml:"enableSubscriberVerification,omitempty"`
 }
 
